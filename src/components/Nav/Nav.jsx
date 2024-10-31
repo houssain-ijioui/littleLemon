f568d25d0948d75f29beb8e541352fb2a6c1fa98
Lahoucine Ijioui <ije1work@gmail.com>
2024-10-31 15:18:45 +0100

    restyle navbar and make it responsive

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import logo from "../../assets/Little-Lemon-Logo-circle-white.png"
+import React, { useEffect, useState } from 'react'
 import './nav.css';
 import { Link } from 'react-router-dom';
 
 
 
 const Nav = () => {
+  const [ showToggle, setShowToggle ] = useState(false);
+
+  const toggleMenu = () => { setShowToggle(!showToggle) }
+
+  useEffect(() => {
+    console.log(showToggle);
+  }, [toggleMenu])
+
   return (
     <nav>
-        <img src={logo}  alt="Little Lemon Logo" />
-        <ul className='links'>
-          <li className='link'><Link to={"/"}>Home</Link></li>
-          <li className='link'><Link to={"/booking"}>Booking</Link></li>
-          <li className='link'><Link to={"/policy"}>Policy</Link></li>
-          <li className='link' id='about-link'><Link to={"/about"}>About Us</Link></li>
-        </ul>
+      <Link to={"/"}><h2 id='logo'>Little Lemon</h2></Link>
+      <button onClick={toggleMenu} id='toggle-menu'>☰</button>
+      <ul className={`links ${showToggle ? 'open-toggle': 'close-toggle'}`} >
+        <li className='link'><Link to={"/"}>Home</Link></li>
+        <li className='link'><Link to={"/booking"}>Booking</Link></li>
+        <li className='link'><Link to={"/policy"}>Policy</Link></li>
+        <li className='link' id='about-link'><Link to={"/about"}>About Us</Link></li>
+      </ul>
     </nav>
   )
 }
